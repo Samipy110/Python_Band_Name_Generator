1273be47083e8da9fb18d281d6ce49148df068af
Abdul Sami <absami2k24@gmail.com>
2024-09-28 03:16:05 +0500

    Add files via upload

---
+++ band_name.py
@@ -0,0 +1,7 @@
+name = input("Enter your city name you grew up:\n")
+# print(name)
+
+pet_name = input("Enter your pet name:\n")
+# print(pet_name)
+
+print("Your band Name could be: " + name + " " + pet_name)
